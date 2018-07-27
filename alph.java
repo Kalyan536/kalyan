80eb8b0590b8f182ef242e8ccc70b152a6404b9c
Kalyan536 <40287658+Kalyan536@users.noreply.github.com>
2018-07-27 12:54:42 +0530

    Update alph.java

--- alph.java
+++ alph.java
@@ -6,15 +6,15 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		 char ch;
-        Scanner scan = new Scanner(System.in);
-        ch = scan.next().charAt(0);
- if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
-        {
-            System.out.print("Alphabet");
-        }
-        else
-        {
-            System.out.print("not an Alphabet");
-        }
+                  Scanner scan = new Scanner(System.in);
+                  ch = scan.next().charAt(0);
+                  if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+                  {
+                      System.out.print("Alphabet");
+                  }
+                   else
+                     {
+                      System.out.print("not an Alphabet");
+                     }
 	}
 }
