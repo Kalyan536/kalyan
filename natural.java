c1c0846e28802da5ee41fdcbd3088d25d89aab34
Kalyan536 <40287658+Kalyan536@users.noreply.github.com>
2018-07-31 14:39:37 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int k,n;
+		Scanner s = new Scanner(System.in);
+		n=s.nextInt();
+		k=(n*(n+1))/2;
+		System.out.println(k);
+		
+	}
+}
