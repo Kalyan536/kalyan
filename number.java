3a29dbb83b5dae3f5d3c069a593ad8d468df76ae
Kalyan536 <40287658+Kalyan536@users.noreply.github.com>
2018-07-31 12:12:01 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int N;
+		int i;
+		Scanner s = new Scanner(System.in);
+		N=s.nextInt();
+		for(i=1;i<=N;i++)
+		System.out.println(N);
+		
+	}
+}
+	
