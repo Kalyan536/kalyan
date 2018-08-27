5d8bd8d1b5666b6c83bda660fb030408f2bb3d2f
Kalyan536 <40287658+Kalyan536@users.noreply.github.com>
2018-08-27 12:42:55 +0530

    Create intervalbwoddnum.java

---
+++ intervalbwoddnum.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int N;
+		int Q;
+		Scanner s = new Scanner(System.in);
+		N=s.nextInt();
+		Q=s.nextInt();
+		if(N<=Q)
+		{
+			for(int i=N+1;i<Q;i++)
+			{
+				if(i%2!=0)
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
